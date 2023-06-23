e706609590dd49f544838a35bd8b88271d6f173d
JavoAlejandro <javier.arriagada@uc.cl>
2023-06-23 18:38:34 -0400

    23/06

--- config.js
+++ config.js
@@ -1,6 +1,22 @@
 const CamerasURL ="https://raw.githubusercontent.com/JavoAlejandro/Proyecto_InfoVis/main/dataset.csv"
 
 d3.csv(CamerasURL).then(function(datos) {
+    for(let i = 0; i < datos.length; i++) {
+        datos[i].Release_date = +datos[i].Release_date;
+        datos[i].Max_resolution = +datos[i].Max_resolution;
+        datos[i].Low_resolution = +datos[i].Low_resolution;
+        datos[i].Effective_pixels = +datos[i].Effective_pixels;
+        datos[i].Zoom_wide = +datos[i].Zoom_wide;
+        datos[i].Zoom_tele = +datos[i].Zoom_tele;
+        datos[i].Normal_focus_range = +datos[i].Normal_focus_range;
+        datos[i].Macro_focus_range = +datos[i].Macro_focus_range;
+        datos[i].Storage_included = +datos[i].Storage_included;
+        datos[i].Weight_incbatteries = +datos[i].Weight_incbatteries;
+        datos[i].Dimensions = parseFloat(datos[i].Dimensions);
+        datos[i].Price = +datos[i].Price;
+    }
     createCameras(datos);
+
+    lineas_marca(datos);
 })
 .catch((error) => console.log(error));
--- main.js
+++ main.js
@@ -1,54 +1,81 @@
 const SVG1 = d3.select("#vis-1").append("svg");
-// const SVG2 = d3.select("#vis-2").append("svg");
+const SVG2 = d3.select("#vis-21").append("svg");
 
 const WIDTH1 = 900;
 const HEIGHT1 = 400;
-// const WIDTH2 = 1100;
-// const HEIGHT2 = 1600;
-// const TIEMPO_TRANSICION = 800;
 const margin = {
     top: 30,
     bottom: 50,
-    left: 120,
-    right: 100,
+    left: 90,
+    right: 220,
   };
-
 const HEIGHTVIS1 = HEIGHT1 - margin.top - margin.bottom;
 const WIDTHVIS1 = WIDTH1 - margin.right - margin.left;
 
+const WIDTH2 = 900;
+const HEIGHT2 = 400;
+const margin2 = {
+  top: 30,
+  bottom: 50,
+  left: 90,
+  right: 100,
+};
+const HEIGHTVIS2 = HEIGHT2 - margin2.top - margin2.bottom;
+const WIDTHVIS2 = WIDTH2 - margin2.right - margin2.left;
 
-SVG1.attr("width", WIDTH1).attr("height", HEIGHT1).style("border", "1px solid black");
-// SVG2.attr("width", WIDTH2).attr("height", HEIGHT2);
+const detail_WIDTH2 = 400
+const detail_HEIGHT2 = 400
+
+const TIEMPO_TRANSICION = 800;
+
+
+
+SVG1.attr("width", WIDTH1).attr("height", HEIGHT1).attr("id", "svg1").style("border", "1px solid black");
+SVG2.attr("width", WIDTH2).attr("height", HEIGHT2).attr("id", "svg2").style("border", "1px solid black");
 
 const grafico1 = SVG1.append("g")
   .attr("id", "grafico1")
   .style("position", "absolute")
-  .style("z-index", "1")
+  .style("z-index", "-1")
   .attr("transform", `translate(${margin.left} ${margin.top})`);
 
+
+const grafico2 = SVG2.append("g")
+  .attr("id", "grafico2")
+  .style("position", "absolute")
+  .style("z-index", "1")
+  .attr("transform", `translate(${margin2.left} ${margin2.top})`);
 
   
 function createCameras(data) {
   // Ejes
   const maximaresolucion = d3.max(data, (d) => d.Max_resolution);
   const maximamplitud = d3.max(data, (d) => d.Zoom_wide);
+  const maximaPrice = d3.max(data, (d) => d.Price);
+  // Price
   console.log(maximaresolucion)
   console.log(maximamplitud)
+  console.log(maximaPrice)
     
-  const escalaX = d3
+  const escala_amplitud = d3
   .scaleLinear()
   .domain([0, maximamplitud])
   .range([0, WIDTHVIS1]);
+
+  const escala_price = d3
+  .scaleLinear()
+  .domain([0, maximaPrice])
+  .range([0, WIDTHVIS1]);
       
      
-  const escalaY = d3
+  const escala_resolucion = d3
   .scaleLinear()
   .domain([0, maximaresolucion])
   .range([HEIGHTVIS1, 0]);
       
     
-  const ejeY = d3.axisLeft(escalaY);
-  const ejeX = d3.axisBottom(escalaX);
+  const ejeY = d3.axisLeft(escala_resolucion);
+  const ejeX = d3.axisBottom(escala_price);
 
   SVG1
   .append("g")
@@ -58,12 +85,13 @@ function createCameras(data) {
     .attr("x1", WIDTHVIS1)
     .attr("stroke-dasharray", "5")
     .attr("opacity", 0.1);
-  // grafico1
-  // .append("text")
-  // .attr("class", "labelxy")
-  // .attr("text-anchor", "end")
-  // .attr("x", margin.left + 300)
-  // .attr("y", margin.top - 20)
+    SVG1
+    .append("text")
+    .attr("class", "labely")
+    .attr("text-anchor", "end")
+    .attr("x", 270)
+    .attr("y", HEIGHTVIS1 +  margin.top+ 75)
+    .text("Camera`s Resolution");
 
   SVG1
   .append("g")
@@ -73,19 +101,262 @@ function createCameras(data) {
     .attr("y1", -HEIGHTVIS1)
     .attr("stroke-dasharray", "5")
     .attr("opacity", 0.1);
-  // grafico1
-  //   .append("text")
-  //   .attr("class", "labelxy")
-  //   .attr("text-anchor", "end")
-  //   .attr("x", WIDTH1 -margin.right)
-  //   .attr("y", HEIGHT1 -10)
-  //   .text("Personas que utilizan actualmente esta herramienta");
+
+  SVG1
+    .append("text")
+    .attr("class", "labelx")
+    .attr("text-anchor", "end")
+    .attr("x", WIDTH1 -margin.right)
+    .attr("y", HEIGHT1 -10)
+    .text("Camera`s Price");
     
-  // grafico1
-  //   .selectAll("circle")
-  //   .data(data).join("circle").attr("class", "disco")
-  //   .attr("r", 6)
-  //   .attr("fill", "white")
-  //   .attr("cx", 50)
-  //   .attr("cy", 50)
+
+  const cuadro = grafico1.append("g").attr("id", "cuadro")
+    .style("visibility", "hidden")
+  const fondo = cuadro
+  .append("rect")
+    .attr("id", "fondo_tt")
+    .style("fill", "yellow")
+    .attr("stroke", "#113149")
+    .attr("stroke-width", "1")
+    .attr("width", 205)
+    .attr("height", 60)
+    .attr("rx", "4px");
+
+  const txt11 = cuadro.append("text").attr("class", "txt");
+  const txt12 = cuadro.append("text").attr("class", "txt");
+  const txt13 = cuadro.append("text").attr("class", "txt");
+
+  grafico1
+    .selectAll("circle")
+    .data(data)
+    .join(
+      (enter) =>
+        enter
+          .append("circle")
+          .attr("class", "punto")
+          .attr("cx", (d, i) => escala_price(d.Price))
+          .attr(
+            "cy", (d) =>escala_resolucion(d.Max_resolution))
+          .attr("r", (d) => 4)
+          .attr("fill", "#D9ED92")
+          .attr("stroke", "#113149"),
+      (update) =>
+        update
+          .transition()
+          .duration(TIEMPO_TRANSICION)
+          .attr(
+            "cy",
+            (d) => 3
+          )
+          ,
+      (exit) => exit.remove()
+    ) .on("mouseover", function (event, d) {
+      txt11.text(`Camera: ${d.Model}`);
+      txt12.text(`Price: ${d.Price}`);
+      txt13.text(`Max Resolution: ${d.Max_resolution}`);
+      cuadro.style("visibility", "visible");
+    })
+    .on("mousemove", function (event) {
+      txt11
+        .attr("x", event.offsetX - 75 + "px")
+        .attr("y", event.offsetY + 20 + "px");
+      txt12
+        .attr("x", event.offsetX - 75 + "px")
+        .attr("y", event.offsetY + 35 + "px");
+      txt13
+        .attr("x", event.offsetX - 75 + "px")
+        .attr("y", event.offsetY + 50 + "px");
+      fondo
+        .attr("x", event.offsetX - 80 + "px")
+        .attr("y", event.offsetY + 5 + "px");
+    })
+    .on("mouseout", () => {
+      cuadro.style("visibility", "hidden");
+    });
+
+};
+
+
+
+
+// ******************************************************************* VIS 2 ************************************************************************************
+
+function calculateQuartiles(numbers) {
+  numbers.sort((a, b) => a - b); 
+
+  const q1Index = Math.floor(numbers.length / 4);
+  const q2Index = Math.floor(numbers.length / 2);
+  const q3Index = Math.floor((3 * numbers.length) / 4);
+
+  const q1 = numbers[q1Index];
+  const q2 = numbers[q2Index];
+  const q3 = numbers[q3Index];
+
+  // console.log(`precios: ${numbers}`)
+
+  const RIQ = q3- q1
+  
+  const LI = q1 - 1.5 *RIQ
+  const LS = q3 + 1.5 *RIQ
+
+  return [q1, q2, q3, d3.max([LI, 0]), d3.max([LS, 0])];
 }
+
+function calculateOutliers(numbers) {
+  numbers.sort((a, b) => a - b); // Sort the numbers in ascending order
+
+  const q1Index = Math.floor(numbers.length / 4);
+  const q3Index = Math.floor((3 * numbers.length) / 4);
+
+  const q1 = numbers[q1Index];
+  const q3 = numbers[q3Index];
+
+  const iqr = q3 - q1;
+  const lowerBound = q1 - (1.5 * iqr);
+  const upperBound = q3 + (1.5 * iqr);
+
+  return numbers.filter(d => d < lowerBound || d > upperBound);
+}
+
+
+
+function lineas_marca(data) {
+  const maximaPrice = d3.max(data, (d) => +d.Price);
+  const grupos = d3.group(data, d => d.Brand);
+  const columns = grupos.size;
+  // console.log(`size: ${grupos.size}`);
+
+  const xScale = d3.scaleBand()
+        .domain(d3.range(columns))
+        .range([0, WIDTHVIS2])
+        .padding(0.1);
+  
+  const escala_y = d3
+        .scaleLinear()
+        .domain([0, 4000])
+        .range([HEIGHTVIS1, 0]);
+
+  const ejeY = d3.axisLeft(escala_y);
+
+  grafico2.call(ejeY)
+    .selectAll("line")
+    .attr("x1", WIDTHVIS2)
+    .attr("stroke-dasharray", "5")
+    .attr("opacity", 0.1);
+
+  var count = 0;
+  for (const [clave, valores] of grupos.entries()) {
+    // console.log(`grupo: ${count+1}, ${clave}`);
+    // Realiza aquí las operaciones que necesites con los resultados
+    const precios = valores.map(d => +d.Price);
+    const minimo = d3.min(precios);
+    const maximo = d3.max(precios);
+
+    const grupoCaja = grafico2.append('g')
+    .attr("transform", `translate(${xScale(count)}, 0)`);
+
+    
+    // Calcular los valores atípicos para el gráfico de caja
+    const valoresAtipicos = calculateOutliers(precios);
+    // console.log(`valoresAtipicos: ${valoresAtipicos}`);
+    
+    
+    const cuartiles = calculateQuartiles(precios);
+    // console.log(`cuartiles: ${cuartiles[0]},${cuartiles[1]},${cuartiles[2]}.LI:${cuartiles[3]},LS:${cuartiles[4]}`);
+
+
+    grupoCaja.selectAll('rect')
+      .data([minimo, cuartiles[0], cuartiles[1], cuartiles[2],cuartiles[3],cuartiles[4], maximo])
+      .join(
+        (enter) => 
+      enter
+      .append('rect')
+      .attr('x', 0)
+      .attr('y', escala_y(cuartiles[2]))
+      .attr('width', 10)
+      .attr('height', Math.abs(escala_y(cuartiles[2]) - escala_y(cuartiles[0])))
+      .attr('stroke', 'black')
+      .attr("fill","transparent")
+      .attr('stroke-width', 2)
+      .attr('class', 'box')
+      ).on("click", (d) => console.log(valores));
+      // (d) => updateBarChart(d)
+    
+    grupoCaja.append('line')
+      .attr('x1', 0)
+      .attr('x2', 10)
+      .attr('y1', (d)=> escala_y(cuartiles[1]))
+      .attr('y2', (d)=> escala_y(cuartiles[1]))
+      .attr('stroke', 'white')
+      .attr('stroke-width', 2)
+    // LI
+    grupoCaja.append('line')
+      .attr('x1', 0)
+      .attr('x2', 10)
+      .attr('y1', (d)=> escala_y(cuartiles[3]))
+      .attr('y2', (d)=> escala_y(cuartiles[3]))
+      .attr('stroke', 'black')
+      .attr('stroke-width', 2)
+
+    // LS
+    grupoCaja.append('line')
+      .attr('x1', 0)
+      .attr('x2', 10)
+      .attr('y1', (d)=> escala_y(cuartiles[4]))
+      .attr('y2', (d)=> escala_y(cuartiles[4]))
+      .attr('stroke', 'black')
+      .attr('stroke-width', 2)
+
+    grupoCaja.append('line')
+      .attr('x1', 5)
+      .attr('x2', 5)
+      .attr('y1', escala_y(cuartiles[4]))
+      .attr('y2', escala_y(cuartiles[2]))
+      .attr('stroke', 'black')
+      .attr('stroke-width', 2)
+    grupoCaja.append('line')
+      .attr('x1', 5)
+      .attr('x2', 5)
+      .attr('y1', escala_y(cuartiles[0]))
+      .attr('y2', escala_y(cuartiles[3]))
+      .attr('stroke', 'black')
+      .attr('stroke-width', 2)
+
+    grupoCaja.selectAll('circle')
+      .data(valoresAtipicos)
+      .join(
+        (enter) => 
+      enter
+      .append('circle')
+      .attr('cx', 5)
+      .attr('cy', d => escala_y(d))
+      .attr('r', 2)
+      .attr('fill', 'red'));
+    
+    grupoCaja.selectAll('text').data([clave])
+      .join(
+        (enter) => 
+      enter.append('text')
+      .attr('x', 5)
+      .attr('y', HEIGHT2 - margin2.bottom)
+      .attr('text-anchor', 'middle')
+      .attr('fill', 'black')
+      .text(clave)
+      .attr('transform', `translate(${5}, ${HEIGHT2 - margin2.bottom}) rotate(-45) translate(-${5}, -${HEIGHT2 - margin2.bottom})`)
+      .attr("class","txt"));
+    count ++;
+  }
+  // .append("svg").attr("width", detail_WIDTH2).attr("height", detail_HEIGHT2).attr("id", "detail2").style("border", "1px solid black");
+  var detail_SVG2 = d3.select("#vis-22");
+  function updateBarChart(data) {
+    d3.select("#vis-22 svg").remove();
+    var detailSvg = d3.select("#vis-22")
+                      .append("svg")
+                      .attr("width", detail_WIDTH2)
+                      .attr("height", detail_HEIGHT2)
+                      .attr("id", "detail2")
+                      .style("border", "1px solid black");
+    console.log(data)
+  }
+};
